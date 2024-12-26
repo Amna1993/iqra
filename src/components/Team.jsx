4e3ef68ac737fd1ef4a2baa654033c3f2896078b
Hafiza Amna Razzaq <hafizaamnarazzaq@Hafizas-MacBook-Pro.local>
2024-12-25 23:14:53 -0500

    team update

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,10 @@ const Team = () => {
   return (
     <section id="team" className="py-20 bg-black">
       <div className="container mx-auto px-6">
-        <h2 className="text-5xl font-extrabold text-center text-yellow-400 mb-12">
+        <h1 className="text-5xl font-extrabold text-center text-yellow-400 mb-12">
+          Team
+        </h1>
+        <h2 className="text-5xl font-extrabold text-center text-white mb-12">
           On the Horizon
         </h2>
         
