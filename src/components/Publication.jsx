0105efcbf8c0ba0b198f03677b75d366c289cae1
Amna Razzaq Awan <87529085+Amna1993@users.noreply.github.com>
2025-01-18 16:17:19 -0500

    Update Publication.jsx

--- src/components/Publication.jsx
+++ src/components/Publication.jsx
@@ -14,15 +14,20 @@ const Publication = () => {
       title: "GitHub",
       link: "https://github.com/iqrabatool",
     },
+    {
+      title: "Web of Science",
+      link: "https://www.webofscience.com/wos/author/record/MAH-1278-2025",
+    },
+    
   ];
 
   return (
-    <section id="publications" className="py-20 bg-black text-white">
+    <section id="publications" className="py-12 bg-black text-white">
       <div className="container mx-auto px-6">
-        <h2 className="text-5xl font-extrabold text-yellow-400 text-center mb-12">
+        <h2 className="text-4xl font-extrabold text-yellow-400 text-center mb-8">
           Publications
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {courses.map((course, index) => (
             <div
               key={index}
