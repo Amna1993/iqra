bd1b6cffa78c485dc453f0f8bdfc89a4c4660c95
Hafiza Amna Razzaq <hafizaamnarazzaq@mac.home>
2025-01-18 15:09:32 -0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Bio from "./components/Bio";
 import Awards from "./components/Awards";
 import Team from "./components/Team";
 import Research from "./components/Research";
+import Education from "./components/Education";
 import Courses from "./components/Courses";
 import Funding from "./components/Funding";
 import Footer from "./components/Footer";
@@ -17,6 +18,7 @@ function App() {
       <Home />
       <Bio />
       <Research />
+      <Education />
       <Publication />
       <Awards />
       <Courses />
